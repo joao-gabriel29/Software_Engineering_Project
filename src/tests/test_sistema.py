10605090eaacff4f19f10063dd0aafe0cc7ee761
João Gabriel <101129joao@gmail.com>
2026-05-30 16:35:15 -0300

    Correção do Código para corrigir o Github Actions

--- src/tests/test_sistema.py
+++ src/tests/test_sistema.py
@@ -2,7 +2,9 @@ from src.sistema import GerenciadorTarefas
 
 def test_criar_tarefa_com_sucesso():
     gerenciador = GerenciadorTarefas()
-    gerenciador.criar_tarefa("Testar Sistema", "Garantir o funcionamento")
-
+    
+    gerenciador.criar_tarefa("Testar Sistema", "Garantir o funcionamento", "Alta")
+    
     assert len(gerenciador.lista_tarefas) == 1
     assert gerenciador.lista_tarefas[0].titulo == "Testar Sistema"
+    assert gerenciador.lista_tarefas[0].prioridade == "Alta"
